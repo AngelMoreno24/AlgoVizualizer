8431707b21749bc12d7579e65e5e18caba774e40
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-02-13 20:00:50 -0700

    create number list generator

--- algovisualizer/src/components/numberGen.js
+++ algovisualizer/src/components/numberGen.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const numberGen = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const numberGen = (size) => {
+
+
+    let array = [];
+    
+    for(let i=0;i<size;i++){
+        
+        array.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1);
+    }
+
+
+    return array;
 }
 
 export default numberGen
